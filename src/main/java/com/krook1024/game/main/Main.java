aa63d7289ce06910d06bdee6c587d8990b120e17
Molnár Antal Albert <krook1024@gmail.com>
2020-04-26 00:15:17 +0200

    Added missing Javadoc to Main::main()

--- src/main/java/com/krook1024/game/main/Main.java
+++ src/main/java/com/krook1024/game/main/Main.java
@@ -6,6 +6,11 @@ import javafx.application.Application;
  * The main class that launches the application GUI.
  */
 public class Main {
+    /**
+     * The method that runs first when the application is started.
+     *
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
         Application.launch(App.class, args);
     }
