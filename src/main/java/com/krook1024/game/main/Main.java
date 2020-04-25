6a4c85a7189ce48c4c46a30dfaee13152316f960
Molnár Antal Albert <krook1024@gmail.com>
2020-04-25 18:04:15 +0200

    Created App and Main classes with a hello world text for now

---
+++ src/main/java/com/krook1024/game/main/Main.java
@@ -0,0 +1,12 @@
+package com.krook1024.game.main;
+
+import javafx.application.Application;
+
+/**
+ * The main class that launches the application GUI.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
