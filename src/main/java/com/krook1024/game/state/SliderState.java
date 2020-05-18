abf9430ff69ce6c8d3d13816e9b40c8ed8aeb1f7
Molnár Antal Albert <krook1024@gmail.com>
2020-05-18 21:28:47 +0200

    Revert "Simplified Tile and SliderState"
    This reverts commit 0d9fbb7d45a74108309116c10ce67c852dd24985.

--- src/main/java/com/krook1024/game/state/SliderState.java
+++ src/main/java/com/krook1024/game/state/SliderState.java
@@ -17,11 +17,11 @@ public class SliderState {
 
     private void initialize() {
         tiles = List.of(
-                new Tile(TileType.TYPE1, new Point(0, 0)),
-                new Tile(TileType.TYPE2, new Point(2, 0)),
-                new Tile(TileType.TYPE3, new Point(0, 2)),
-                new Tile(TileType.TYPE4, new Point(2, 2)),
-                new Tile(TileType.TYPE5, new Point(4, 2))
+                new Tile(TileType.TYPE1, new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(0, 1)),
+                new Tile(TileType.TYPE2, new Point(2, 0), new Point(3, 0), new Point(3, 1), new Point(3, 1)),
+                new Tile(TileType.TYPE3, new Point(0, 2), new Point(0, 2), new Point(0, 3), new Point(1, 3)),
+                new Tile(TileType.TYPE4, new Point(3, 2), new Point(3, 2), new Point(2, 3), new Point(3, 3)),
+                new Tile(TileType.TYPE5, new Point(4, 2), new Point(5, 2), new Point(4, 3), new Point(5, 3))
         );
     }
 
