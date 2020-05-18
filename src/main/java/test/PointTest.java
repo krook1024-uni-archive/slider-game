4de342d8e171b1cf4d5f1442a54d4d060bc9bc80
Molnár Antal Albert <krook1024@gmail.com>
2020-05-18 21:59:20 +0200

    Added unit tests for Point

---
+++ src/main/java/test/PointTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import com.krook1024.game.state.Point;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void distanceTo() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 1);
+        Point c = new Point(1, 0);
+        Point d = new Point(1, 1);
+
+        assertEquals(0, a.distanceTo(a));
+        assertEquals(1, a.distanceTo(b));
+        assertEquals(1, a.distanceTo(1, 0));
+        assertEquals(1, a.distanceTo(c));
+        assertEquals(1.4142, a.distanceTo(d), 0.001);
+    }
+}
