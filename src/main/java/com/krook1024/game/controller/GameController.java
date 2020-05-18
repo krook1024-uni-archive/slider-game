a6ee6f6e7cce911063be961b2eeb394a1d30cd47
Molnár Antal Albert <krook1024@gmail.com>
2020-05-18 16:56:02 +0200

    Implemented GameController to show a gamestate

--- src/main/java/com/krook1024/game/controller/GameController.java
+++ src/main/java/com/krook1024/game/controller/GameController.java
@@ -1,18 +1,27 @@
 package com.krook1024.game.controller;
 
+import com.krook1024.game.state.SliderState;
+import com.krook1024.game.state.Tile;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 import java.time.LocalTime;
+import java.util.List;
 
 /**
  * Acts as a controller class for the game view.
@@ -20,11 +29,17 @@ import java.time.LocalTime;
 public class GameController extends BaseController {
     private String name;
     private Duration time = Duration.ZERO;
+    private IntegerProperty steps = new SimpleIntegerProperty(0);
+    private SliderState sliderState;
+    private Timeline clock;
+    private List<Image> images;
 
     @FXML
     Label usernameLabel;
     @FXML
     Label elapsedTimeLabel;
+    @FXML
+    Label stepsLabel;
     @FXML
     GridPane gameGrid;
 
@@ -34,14 +49,29 @@ public class GameController extends BaseController {
 
     @FXML
     private void initialize() {
+        images = List.of(
+                new Image(getClass().getResource("/rectangle/1.png").toExternalForm()),
+                new Image(getClass().getResource("/rectangle/2.png").toExternalForm()),
+                new Image(getClass().getResource("/rectangle/3.png").toExternalForm()),
+                new Image(getClass().getResource("/rectangle/4.png").toExternalForm()),
+                new Image(getClass().getResource("/rectangle/5.png").toExternalForm())
+        );
+        //stepsLabel.textProperty().bind(steps.asString());
+        resetGame();
+    }
+
+    private void resetGame() {
         Platform.runLater(() -> {
             logger.info("Setting name to {}", name);
             usernameLabel.setText("Hello, " + name);
         });
-
-        Timeline clock = getClockTimeline();
+        sliderState = new SliderState();
+        steps.set(0);
+        time = Duration.ZERO;
+        clock = getClockTimeline();
         clock.setCycleCount(Animation.INDEFINITE);
         clock.play();
+        draw();
     }
 
     /**
@@ -68,5 +98,19 @@ public class GameController extends BaseController {
      */
     private String formatElapsedTime(int seconds) {
         return String.format("%d:%02d:%02d", seconds / 3600, (seconds % 3600) / 60, (seconds % 60));
+    }
+
+    public void draw() {
+        for (Tile t : sliderState.getTiles()) {
+            ImageView imageView = new ImageView();
+            GridPane.setRowIndex(imageView, Math.min(t.getTopLeft().getY(), t.getBotLeft().getY()));
+            GridPane.setColumnIndex(imageView, Math.min(t.getTopRight().getX(), t.getBotRight().getX()));
+
+            GridPane.setRowSpan(imageView, 2);
+            GridPane.setColumnSpan(imageView, 2);
+
+            imageView.setImage(images.get(t.getType().getValue() - 1));
+            gameGrid.getChildren().add(imageView);
+        }
     }
 }
