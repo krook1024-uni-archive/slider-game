ccd686130bdc29210bba63ab11f47a1453bb568a
Molnár Antal Albert <krook1024@gmail.com>
2020-05-18 10:38:47 +0200

    Initialized GameController

--- src/main/java/com/krook1024/game/controller/GameController.java
+++ src/main/java/com/krook1024/game/controller/GameController.java
@@ -1,7 +1,32 @@
 package com.krook1024.game.controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
 /**
  * Acts as a controller class for the game view.
  */
 public class GameController extends BaseController {
+    private String name;
+
+    @FXML
+    Label usernameLabel;
+    @FXML
+    Label elapsedTime;
+    @FXML
+    GridPane gameGrid;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @FXML
+    private void initialize() {
+        Platform.runLater(() -> {
+            logger.info("Setting name to {}", name);
+            usernameLabel.setText("Hello, " + name);
+        });
+    }
 }
