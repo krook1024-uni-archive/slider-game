518488efbc629a4625b9560a0fb4380ecc16e480
Molnár Antal Albert <krook1024@gmail.com>
2020-05-19 11:44:15 +0200

    Updated tests according to the previous commit

--- src/main/java/test/TileTest.java
+++ src/main/java/test/TileTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import com.krook1024.game.state.Axis;
 import com.krook1024.game.state.Point;
 import com.krook1024.game.state.Tile;
 import com.krook1024.game.state.TileType;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.stream.IntStream;
 
+import static com.krook1024.game.state.Axis.X;
 import static com.krook1024.game.state.Direction.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,26 +34,26 @@ class TileTest {
     @Test
     void stepAcrossX() {
         Tile tile = new Tile(TileType.TYPE1, new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(0, 1));
-        tile.stepAcrossX(RIGHT);
+        tile.step(RIGHT, Axis.X);
         assertEquals(new Tile(TileType.TYPE1, new Point(1, 0), new Point(2, 0), new Point(1, 1), new Point(1, 1)), tile);
-        tile.stepAcrossX(LEFT);
+        tile.step(LEFT, Axis.X);
         assertEquals(new Tile(TileType.TYPE1, new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(0, 1)), tile);
-        tile.stepAcrossX(LEFT);
+        tile.step(LEFT, Axis.X);
         assertEquals(new Tile(TileType.TYPE1, new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(0, 1)), tile);
-        IntStream.range(0, 100).mapToObj(i -> RIGHT).forEach(tile::stepAcrossX);
+        IntStream.range(0, 100).forEach(i -> tile.step(RIGHT, X));
         assertEquals(new Tile(TileType.TYPE1, new Point(4, 0), new Point(5, 0), new Point(4, 1), new Point(4, 1)), tile);
     }
 
     @Test
     void stepAcrossY() {
         Tile tile = new Tile(TileType.TYPE1, new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(0, 1));
-        tile.stepAcrossY(DOWN);
+        tile.step(DOWN, Axis.Y);
         assertEquals(new Tile(TileType.TYPE1, new Point(0, 1), new Point(1, 1), new Point(0, 2), new Point(0, 2)), tile);
-        tile.stepAcrossY(UP);
+        tile.step(UP, Axis.Y);
         assertEquals(new Tile(TileType.TYPE1, new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(0, 1)), tile);
-        tile.stepAcrossY(UP);
+        tile.step(UP, Axis.Y);
         assertEquals(new Tile(TileType.TYPE1, new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(0, 1)), tile);
-        IntStream.range(0, 100).mapToObj(i -> DOWN).forEach(tile::stepAcrossY);
+        IntStream.range(0, 100).forEach(i -> tile.step(RIGHT, X));
         assertEquals(new Tile(TileType.TYPE1, new Point(0, 2), new Point(1, 2), new Point(0, 3), new Point(0, 3)), tile);
     }
 }
